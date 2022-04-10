bfc9472b98c84b3b35ebc29b2068ee61a0556f61
Ross Duquette <ross@skygauge.co>
2022-04-10 13:24:21 -0400

    Update main.cpp to use Encoder and Decoder objects

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 
 #include "arg_parser.h"
+#include "binary_csv_decoder.h"
+#include "binary_csv_encoder.h"
 
 int main(int argc, const char* argv[])
 {
@@ -9,10 +11,19 @@ int main(int argc, const char* argv[])
         printf("%s\n", arg_parser.get_help());
         return 1;
     }
-    printf("cmd: %d\ninfile: %s\noutfile: %s\ndelete: %d\n",
-           arg_parser.get_arg_cmd(),
-           arg_parser.get_input_filename(),
-           arg_parser.get_output_filename(),
-           arg_parser.delete_input_file());
+
+    bool success = false;
+    if (arg_parser.get_arg_cmd() == ARG_CMD_ENCODE) {
+        BinaryCsvEncoder encoder;
+        success = encoder.process(arg_parser.get_input_filename(),
+                                  arg_parser.get_output_filename());
+    } else if (arg_parser.get_arg_cmd() == ARG_CMD_DECODE) {
+        BinaryCsvDecoder decoder;
+        success = decoder.process(arg_parser.get_input_filename(),
+                                  arg_parser.get_output_filename());
+    }
+    if (success && arg_parser.delete_input_file()) {
+        std::remove(arg_parser.get_input_filename());
+    }
     return 0;
 }
